8f1cb09edfa34a50ce35f08dac60cf3538d8b970
scab24 <git.seco@protonmail.com>
2024-04-25 17:45:11 +0200

    division_before_multiplication

--- aderyn_core/src/detect/detector.rs
+++ aderyn_core/src/detect/detector.rs
@@ -15,10 +15,11 @@ use crate::{
             ContractsWithTodosDetector, DeprecatedOZFunctionsDetector, EcrecoverDetector,
             EmptyBlockDetector, InconsistentTypeNamesDetector, LargeLiteralValueDetector,
             NonReentrantBeforeOthersDetector, OperationOrderDetector, PushZeroOpcodeDetector,
-            RequireWithStringDetector, SolmateSafeTransferLibDetector, UnindexedEventsDetector,
-            UnsafeERC20FunctionsDetector, UnsafeERC721MintDetector,
-            UnspecificSolidityPragmaDetector, UselessInternalFunctionDetector,
-            UselessModifierDetector, UselessPublicFunctionDetector, ZeroAddressCheckDetector,
+            RequireWithStringDetector, RevertsAndRequiresInLoopsDetector,
+            SolmateSafeTransferLibDetector, UnindexedEventsDetector, UnsafeERC20FunctionsDetector,
+            UnsafeERC721MintDetector, UnspecificSolidityPragmaDetector,
+            UselessInternalFunctionDetector, UselessModifierDetector,
+            UselessPublicFunctionDetector, ZeroAddressCheckDetector,
         },
     },
 };
@@ -56,6 +57,7 @@ pub fn get_all_issue_detectors() -> Vec<Box<dyn IssueDetector>> {
         Box::<ContractsWithTodosDetector>::default(),
         Box::<InconsistentTypeNamesDetector>::default(),
         Box::<UnprotectedInitializerDetector>::default(),
+        Box::<RevertsAndRequiresInLoopsDetector>::default(),
         Box::<OperationOrderDetector>::default(),
     ]
 }
@@ -93,16 +95,8 @@ pub(crate) enum IssueDetectorNamePool {
     ContractWithTodos,
     InconsistentTypeNames,
     UnprotectedInitializer,
-    // NOTE: `Undecided` will be the default name (for new bots).
-    // If it's accepted, a new variant will be added to this enum before normalizing it in aderyn
+    RevertsAndRequiresInLoops,
     OperationOrderDetector,
-    Undecided,
-}
-
-#[derive(Debug, PartialEq, EnumString, Display)]
-#[strum(serialize_all = "kebab-case")]
-pub(crate) enum ResuableDetectorNamePool {
-    IdentifiersThatReferenceAFunction,
     // NOTE: `Undecided` will be the default name (for new bots).
     // If it's accepted, a new variant will be added to this enum before normalizing it in aderyn
     Undecided,
@@ -175,6 +169,9 @@ pub fn request_issue_detector_by_name(detector_name: &str) -> Option<Box<dyn Iss
         IssueDetectorNamePool::UnprotectedInitializer => {
             Some(Box::<UnprotectedInitializerDetector>::default())
         }
+        IssueDetectorNamePool::RevertsAndRequiresInLoops => {
+            Some(Box::<RevertsAndRequiresInLoopsDetector>::default())
+        }
         IssueDetectorNamePool::OperationOrderDetector => {
             Some(Box::<OperationOrderDetector>::default())
         }
@@ -238,6 +235,50 @@ pub mod detector_test_helpers {
         context::workspace_context::WorkspaceContext, framework::foundry::read_foundry_output_file,
         read_file_to_string, visitor::ast_visitor::Node,
     };
+
+    pub fn load_multiple_contracts(filepaths: Vec<&str>) -> WorkspaceContext {
+        let mut context = WorkspaceContext::default();
+
+        for filepath in filepaths {
+            let path_buf_filepath = std::path::PathBuf::from(filepath);
+            let foundry_output =
+                read_foundry_output_file(path_buf_filepath.to_str().unwrap()).unwrap();
+            let mut ast = foundry_output.ast.clone();
+            // Get the path of the source file
+            let mut new_path = PathBuf::new();
+            for component in path_buf_filepath.components() {
+                if component.as_os_str() == "out" {
+                    break;
+                }
+                new_path.push(component);
+            }
+            new_path.push(ast.absolute_path.as_ref().unwrap());
+            match read_file_to_string(&new_path) {
+                Ok(content) => {
+                    println!(
+                        "Loaded Solidity source file: {}",
+                        new_path.to_str().unwrap()
+                    );
+
+                    ast.source = Some(content);
+                }
+                Err(err) => {
+                    eprintln!(
+                        "Error reading Solidity source file: {}",
+                        new_path.to_str().unwrap()
+                    );
+                    eprintln!("{:?}", err);
+                }
+            }
+            ast.accept(&mut context).unwrap_or_else(|err| {
+                // Exit with a non-zero exit code
+                eprintln!("Error loading Hardhat AST into WorkspaceContext");
+                eprintln!("{:?}", err);
+            });
+        }
+
+        context
+    }
 
     pub fn load_contract(filepath: &str) -> WorkspaceContext {
         let path_buf_filepath = std::path::PathBuf::from(filepath);
